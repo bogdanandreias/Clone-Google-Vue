40e537ce233a8df19bb3cc9a52a4c7794c81bb0b
AzureAD\Bogdan-IoanAndreias <bogdanandreias1923@gmail.com>
2021-05-05 12:15:41 +0300

    Header created

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <Header />
 </template>
 
 <script>
+import Header from './components/Header';
+// import Header from './components/Header.vue';
+// import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="google-header">
+    <div>
+      <img
+        class="img"
+        src="../assets/me.jpeg"
+        title="Google-Account: bogdanandreias1923@gmail.com"
+      />
+    </div>
+    <div class="header-icons">
+      <i class="material-icons">apps</i>
+      <a href="#">Images</a>
+      <a href="#">Gmail</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+.google-header {
+  width: 100%;
+  max-height: 200px;
+  float: right;
+  font-family: sans-serif;
+}
+
+.google-header img {
+  max-width: 100%;
+  border-radius: 50%;
+}
+
+.img {
+  border-radius: 50%;
+  width: 32px;
+  float: right;
+  margin-right: 15px;
+  margin-top: 20px;
+  cursor: pointer;
+  margin-top: 24px;
+}
+
+.header-icons {
+  float: right;
+  width: 200px;
+  margin-top: 28px;
+  margin-right: 10px;
+}
+
+.header-icons .material-icons {
+  font-size: 25px;
+  float: right;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.header-icons .material-icons:hover {
+  background-color: rgb(172, 172, 172);
+  border-radius: 50%;
+}
+
+.header-icons a {
+  float: right;
+  margin-right: 15px;
+  color: rgb(53, 52, 52);
+  text-decoration: none;
+  font-size: 14px;
+}
+
+.header-icons a:hover {
+  color: gray;
+}
+</style>>
